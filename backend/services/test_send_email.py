c05d4b7281cf34a88ade0de888e0267317363c70
Pahul1anviam <pahuljot.singh@anviam.com>
2025-07-18 13:42:28 +0530

    Update test_send_email.py

--- backend/services/test_send_email.py
+++ backend/services/test_send_email.py
@@ -5,7 +5,7 @@ from datetime import date
 today = date.today().strftime("%Y-%m-%d")
 free_slots = get_available_slots(today)
 
-candidate_email = "psingh1_be21@thapar.edu"
-candidate_name = "Pahuljot Singh"
+candidate_email = "example"
+candidate_name = "example"
 
 send_slot_email(candidate_email, candidate_name, free_slots[:])  # Send only top 5 slots
